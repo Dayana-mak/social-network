0d8e31c22669725a5a24bdac58352d6036c946cf
Daiana Makushina <dayana.makushina@mail.ru>
2025-03-17 15:53:24 +0100

    FEAT: add TS types for forms validators

--- src/utils/validators/validators.ts
+++ src/utils/validators/validators.ts
@@ -1,20 +1,9 @@
 import * as Yup from "yup";
 
-/* export const required = (value) => {
-  if (value) return undefined;
-
-  return "Это поле обязательно"
-}
-
-export const maxLengthConstructor = (maxLength) => (value) => {
-    if (value.length > maxLength) return `Максимальная длина ${maxLength} символов`;
-    return undefined;
-  }
- */
 export const requiredField = Yup.string().required("This field is required");
 export const validEmail = Yup.string().email("Invalid email address").required("This field is required");
 
-export const maxLengthConstructor = (maxLength) => {
+export const maxLengthConstructor = (maxLength: number) => {
   return Yup.string().max(maxLength, `Max length ${maxLength} symbols`)
 }
 
@@ -22,7 +11,7 @@ export const loginValidation = Yup.object({
   email: validEmail,
   password: requiredField,
   captcha: Yup.string().when("$captchaUrl", {
-    is: (captchaUrl) => !!captchaUrl,
+    is: (captchaUrl: any) => !!captchaUrl,
     then: () => Yup.string().required("This field is required"),
     otherwise: () => Yup.string()
   })
